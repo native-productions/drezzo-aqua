336a2aee7cc1e66fe1b2cc231d6e8b915c177c1d
Aryasena <foryoublueee@gmail.com>
2023-09-12 22:38:08 +0700

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: './jsconfig.json',
+      },
+    },
+  },
+  plugins: ['react', 'import'],
+  rules: {
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'no-param-reassign': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/require-default-props': 'off',
+    'no-nested-ternary': 'off',
+    'no-plusplus': 'off',
+    'no-await-in-loop': 'off',
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+}
---
+++ app/lib/utils/view.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/prefer-default-export */
+
+import { clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs))
+}
