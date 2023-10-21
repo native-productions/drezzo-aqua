78c98ed6060afccdac1ef38f853b603d269fcf9d
shewaslatte <92186264+shewaslatte@users.noreply.github.com>
2023-10-22 00:39:43 +0700

    adjust api

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -2,13 +2,25 @@
 import { prisma } from '@/lib/server/prisma'
 
 export async function GET(req) {
-  const password = req.headers.get('password')
-  const query = req.url.split('?')[1]
+  const password = req?.headers?.get('password')
+  const query = req?.url?.split('?')[1]
   const params = new URLSearchParams(query)
+  let page
+  let limit
 
-  const page = Number(params.get('page')) || 1
-  const limit = Number(params.get('limit')) || 10
-  const order = params.get('order').toLowerCase() || 'desc'
+  if (
+    !query ||
+    params?.get('page') === 'undefined' ||
+    params?.get('limit') === 'undefined'
+  ) {
+    page = 1
+    limit = 10
+  } else {
+    page = Number(params?.get('page'))
+    limit = Number(params?.get('limit'))
+  }
+
+  const order = params.get('order')?.toLowerCase() || 'desc'
 
   if (password !== (process.env.HEADER_PASSWORD || 'Drezzo@@123')) {
     return Response.json(
